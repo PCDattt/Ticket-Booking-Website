25c846b2505415f4ed784efddcef095fe791ec3e
Phù Chí Đạt <83968308+PCDattt@users.noreply.github.com>
2023-04-03 08:23:54 +0700

    Initialization
    Add models

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ticket_Booking_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
