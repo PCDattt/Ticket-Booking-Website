2733cd9fd6cc5581a5d7dc67c2591a174a20abf3
Phù Chí Đạt <83968308+PCDattt@users.noreply.github.com>
2023-04-03 08:22:05 +0700

    Initialization
    Add Actors, Cinemas, Movies, Producers controllers

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ticket_Booking_Website.Data;
+
+namespace Ticket_Booking_Website.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ Controllers/CinemasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ticket_Booking_Website.Data;
+
+namespace Ticket_Booking_Website.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            return View(allCinemas);
+        }
+    }
+}
