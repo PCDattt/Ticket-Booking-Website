4750f39c8ae5117465ec67996b3b8ad3db3e6f9f
Phù Chí Đạt <83968308+PCDattt@users.noreply.github.com>
2023-04-03 08:22:37 +0700

    Initialization

---
+++ Data/Enums/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace Ticket_Booking_Website.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Horror,
+        Cartoon
+    }
+}
