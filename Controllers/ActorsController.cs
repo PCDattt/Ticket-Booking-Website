16f1f89decaa78acecdd325575c4e92d52fd2770
PCDattt <datkg1097@gmail.com>
2023-04-04 20:54:07 +0700

    Add update, delete actor action.

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -26,20 +26,54 @@ namespace Ticket_Booking_Website.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([Bind("FullName, ProfilePictureURL, Bio")]Actor actor)
         {
-            if(!ModelState.IsValid)
-            {
-                return View(actor);
-            }
-            await _service.AddAsync(actor);
-            return RedirectToAction(nameof(Index));
-        }
+			if (!ModelState.IsValid)
+			{
+				await _service.AddAsync(actor);
+				return RedirectToAction(nameof(Index));
+			}
+			return View(actor);
+		}
 
         public async Task<IActionResult> Details(int id)
         {
             var actorDetails = await _service.GetByIdAsync(id);
 
-            if (actorDetails == null) return View("Empty");
+            if (actorDetails == null) return View("NotFound");
             return View(actorDetails);
         }
-    }
+
+		public async Task<IActionResult> Edit(int id)
+		{
+			var actorDetails = await _service.GetByIdAsync(id);
+
+			if (actorDetails == null) return View("NotFound");
+			return View(actorDetails);
+		}
+		[HttpPost]
+		public async Task<IActionResult> Edit(int id, Actor actor)
+		{
+			if (!ModelState.IsValid)
+			{
+				await _service.UpdateAsync(id, actor);
+				return RedirectToAction(nameof(Index));
+			}
+			return View(actor);
+		}
+		public async Task<IActionResult> Delete(int id)
+		{
+			var actorDetails = await _service.GetByIdAsync(id);
+
+			if (actorDetails == null) return View("NotFound");
+			return View(actorDetails);
+		}
+		[HttpPost, ActionName("Delete")]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var actorDetails = await _service.GetByIdAsync(id);
+			if (actorDetails == null) return View("NotFound");
+
+			await _service.DeleteAsync(id);
+			return RedirectToAction(nameof(Index));
+		}
+	}
 }
