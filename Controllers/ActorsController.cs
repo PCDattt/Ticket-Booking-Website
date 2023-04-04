a2feebb7c8307764b0374fc103fe572f6f7f0b38
PCDattt <datkg1097@gmail.com>
2023-04-04 09:40:36 +0700

    Add, implement and register IActorsService.

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -1,19 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Ticket_Booking_Website.Data;
+using Ticket_Booking_Website.Data.Services;
 
 namespace Ticket_Booking_Website.Controllers
 {
     public class ActorsController : Controller
     {
-        private readonly AppDbContext _context;
+        private readonly IActorsService _service;
 
-        public ActorsController(AppDbContext context)
+        public ActorsController(IActorsService service)
         {
-            _context = context;
+            _service = service;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var data = _context.Actors.ToList();
+            var data = await _service.GetAll();
             return View(data);
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Ticket_Booking_Website.Data;
+using Ticket_Booking_Website.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IActorsService, ActorsService>();
 
 var app = builder.Build();
 
