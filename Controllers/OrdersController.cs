8144da7be43c17b449d1bcbf8e5a75776734bc38
PCDattt <datkg1097@gmail.com>
2023-04-12 11:50:35 +0700

    Add shopping cart view

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ticket_Booking_Website.Data.Cart;
+using Ticket_Booking_Website.Data.Services;
+using Ticket_Booking_Website.Data.ViewModels;
+
+namespace Ticket_Booking_Website.Controllers
+{
+	public class OrdersController : Controller
+	{
+		private readonly IMoviesService _moviesService;
+		private readonly ShoppingCart _shoppingCart;
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+			_moviesService = moviesService;
+			_shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+		{
+			var items = _shoppingCart.GetShoppingCartItems();
+			_shoppingCart.ShoppingCartItems = items;
+			var response = new ShoppingCartVM()
+			{
+				ShoppingCart = _shoppingCart,
+				ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+			};
+			return View(response);
+		}
+	}
+}
---
+++ Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using Ticket_Booking_Website.Data.Cart;
+
+namespace Ticket_Booking_Website.Data.ViewModels
+{
+	public class ShoppingCartVM
+	{
+		public ShoppingCart ShoppingCart { get; set; }
+		public double ShoppingCartTotal { get; set; }
+	}
+}
